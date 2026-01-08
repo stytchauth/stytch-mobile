384210528db1babe9f732a72adc4eac18f737826
Jordan Haven <jhaven@stytch.com>
2026-01-08 11:56:51 -0500

    Add TODO markers for deviceinfo for JS and JVM platforms

--- stytch-multiplatform/sdk/shared/src/jsMain/kotlin/com/stytch/sdk/data/StytchClientConfiguration.js.kt
+++ stytch-multiplatform/sdk/shared/src/jsMain/kotlin/com/stytch/sdk/data/StytchClientConfiguration.js.kt
@@ -6,6 +6,7 @@ public actual class StytchClientConfiguration(
     internal val publicToken: String,
     internal val endpointOptions: EndpointOptions = EndpointOptions(),
 ) {
+    // TODO: DeviceInfo for JS
     public actual fun toInternal(): StytchClientConfigurationInternal =
         StytchClientConfigurationInternal(
             publicToken = publicToken,
--- stytch-multiplatform/sdk/shared/src/jvmMain/kotlin/com/stytch/sdk/data/StytchClientConfiguration.jvm.kt
+++ stytch-multiplatform/sdk/shared/src/jvmMain/kotlin/com/stytch/sdk/data/StytchClientConfiguration.jvm.kt
@@ -4,6 +4,7 @@ public actual class StytchClientConfiguration(
     internal val publicToken: String,
     internal val endpointOptions: EndpointOptions = EndpointOptions(),
 ) {
+    // TODO: DeviceInfo for JVM
     public actual fun toInternal(): StytchClientConfigurationInternal =
         StytchClientConfigurationInternal(
             publicToken = publicToken,
