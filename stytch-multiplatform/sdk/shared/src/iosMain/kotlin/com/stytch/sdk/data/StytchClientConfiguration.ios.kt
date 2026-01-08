8e25ede9ee8e6cddf01089f22069c884bbe618f3
Jordan Haven <jhaven@stytch.com>
2026-01-07 23:16:37 -0500

    Real DeviceInfo on iOS to validate network requests

--- stytch-multiplatform/sdk/shared/src/iosMain/kotlin/com/stytch/sdk/data/StytchClientConfiguration.ios.kt
+++ stytch-multiplatform/sdk/shared/src/iosMain/kotlin/com/stytch/sdk/data/StytchClientConfiguration.ios.kt
@@ -1,5 +1,12 @@
 package com.stytch.sdk.data
 
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.useContents
+import platform.Foundation.NSBundle
+import platform.Foundation.NSProcessInfo
+import platform.UIKit.UIDevice
+import platform.UIKit.UIScreen
+
 public actual class StytchClientConfiguration(
     public actual val publicToken: String,
     public actual val endpointOptions: EndpointOptions = EndpointOptions(),
@@ -7,14 +14,16 @@ public actual class StytchClientConfiguration(
     public constructor(publicToken: String) : this(publicToken, EndpointOptions())
 
     public actual val isTestToken: Boolean
+
+    @OptIn(ExperimentalForeignApi::class)
     internal actual val deviceInfo: DeviceInfo =
         DeviceInfo(
-            applicationPackageName = "TODO()",
-            applicationVersion = "TODO()",
-            osName = "TODO()",
-            osVersion = "TODO()",
-            deviceName = "TODO()",
-            screenSize = "TODO()",
+            applicationPackageName = NSBundle.mainBundle.bundleIdentifier ?: "unknown_bundle_id",
+            applicationVersion = NSBundle.mainBundle.getVersion(),
+            osName = UIDevice.currentDevice.systemName,
+            osVersion = NSProcessInfo.processInfo.operatingSystemVersionString,
+            deviceName = UIDevice.currentDevice.model.lowercase(),
+            screenSize = UIScreen.mainScreen.bounds.useContents { "(${size.width},${size.height})" },
         )
 
     init {
@@ -23,3 +32,10 @@ public actual class StytchClientConfiguration(
         isTestToken = matches.groupValues[1] == "test"
     }
 }
+
+private fun NSBundle.getVersion(): String =
+    infoDictionary
+        ?.let {
+            // First, try to get the user-visible version, fallback to the internal identifier
+            it["CFBundleShortVersionString"] ?: it["CFBundleVersion"]
+        }.toString()
