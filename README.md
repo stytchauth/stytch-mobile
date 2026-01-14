de9072942319fc60078a927143547c8b6b1604d7
Jordan Haven <jhaven@stytch.com>
2026-01-14 12:33:43 -0500

    make build script nicer/faster

--- README.md
+++ README.md
@@ -5,11 +5,10 @@ This repo publishes:
 * An XCFramework that gets copied to a separate stytch-ios repo (that repo would then ONLY include a `Package.swift` and the `.xcframework` file)
 
 **While testing**
-* Run `./buildArtifactsForTesting.sh`, this will build all three platforms and copy the artifacts to the `artifacts/` directory, which the apps in `examples/` consume
+* Run `./build.sh [platform]`, this will build the specified (or all) platforms and copy the artifacts to the `artifacts/` directory, which the apps in `examples/` consume
 * Run the apps in the `examples/` directory as appropriate to test things
 
 **Known things**
-on ios, classes exported in the shared module are being prepended with "shared" (ie: StytchClientConfiguration -> SharedStytchClientConfiguration). why?
 on ios, default constructor arguments are a bear (see params in loginOrCreate()). If we're codegening, we can just codegen all of the constructors (see StytchClientConfiguration.ios.kt), I guess?
 
 **Big TODOs**
---
+++ build.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+PLATFORM=$1
+ROOT_DIR=`pwd`
+rm -rf artifacts
+mkdir -p artifacts
+cd $ROOT_DIR/stytch-multiplatform
+./gradlew clean
+
+buildAndCopyAndroid() {
+    cd $ROOT_DIR/stytch-multiplatform
+    echo "Building android artifact..."
+    ./gradlew :sdk:consumer-headless:publishAllPublicationsToTESTINGRepository :sdk:consumer-extensions:publishAllPublicationsToTESTINGRepository :sdk:shared:publishAllPublicationsToTESTINGRepository
+}
+
+buildAndCopyIos() {
+    cd $ROOT_DIR/stytch-multiplatform
+    echo "Building iOS library..."
+    cd sdk/shared/src/iosMain/interop
+
+    # clear out any old stuff
+    rm -rf iOS.xcarchive libStytch* Simulator.xcarchive StytchShared
+
+    # create header file (for cinterop generation)
+    sudo swiftc -emit-objc-header Sources/StytchShared/StytchShared.swift
+
+    # build xcarchives for platforms
+    xcodebuild archive -scheme StytchShared -configuration Release -destination 'generic/platform=iOS' -archivePath "./iOS" SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES &&
+    xcodebuild archive -scheme StytchShared -configuration Release -destination 'generic/platform=iOS Simulator' -archivePath "./Simulator" SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+
+    # convert the objects into libraries
+    ar -crs libStytchIos.a ./iOS.xcarchive/Products/Users/jhaven/Objects/StytchShared.o && 
+    ar -crs libStytchSimulator.a ./Simulator.xcarchive/Products/Users/jhaven/Objects/StytchShared.o
+
+    # back to stytch-multiplatform
+    cd $ROOT_DIR/stytch-multiplatform
+ 
+    echo "Building iOS artifact..."
+    ./gradlew assembleStytchConsumerSDKXCFramework assembleStytchConsumerExtensionsSDKXCFramework assembleStytchSharedSDKXCFramework
+    cp -r sdk/consumer-headless/build/XCFrameworks/release/StytchConsumerSDK.xcframework artifacts
+}
+
+buildAndCopyRn() {
+    cd $ROOT_DIR/stytch-multiplatform
+    echo "Building RN artifacts..."
+    ./gradlew :sdk:consumer-headless:jsBrowserProductionLibraryDistribution
+    cp -r sdk/consumer-headless/build/dist/js/productionLibrary artifacts
+}
+
+
+if [ -z "$PLATFORM" ]; then
+    echo "Building for all platforms"
+    buildAndCopyAndroid
+    buildAndCopyIos
+    buildAndCopyRn
+elif [[ "$PLATFORM" == "android" ]]; then
+    buildAndCopyAndroid
+elif [[ "$PLATFORM" == "ios" ]]; then
+    buildAndCopyIos
+elif [[ "$PLATFORM" == "rn" ]]; then
+    buildAndCopyRn
+fi
+
+echo "Done."
