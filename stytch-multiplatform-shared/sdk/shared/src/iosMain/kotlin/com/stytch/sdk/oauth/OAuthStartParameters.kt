12a90ca7a758c04fcb31fb3faddff927f568284c
Jordan Haven <jhaven@stytch.com>
2026-03-05 18:28:10 -0500

    Add secondary constructor helpers for iOS

--- stytch-multiplatform-shared/sdk/shared/src/iosMain/kotlin/com/stytch/sdk/oauth/OAuthStartParameters.kt
+++ stytch-multiplatform-shared/sdk/shared/src/iosMain/kotlin/com/stytch/sdk/oauth/OAuthStartParameters.kt
@@ -4,12 +4,58 @@ import platform.AuthenticationServices.ASAuthorizationControllerPresentationCont
 import platform.AuthenticationServices.ASWebAuthenticationPresentationContextProvidingProtocol
 
 public actual class OAuthStartParameters(
-    public val applePresentationContextProvider: ASAuthorizationControllerPresentationContextProvidingProtocol? = null,
-    public val oauthPresentationContextProvider: ASWebAuthenticationPresentationContextProvidingProtocol? = null,
     public actual val loginRedirectUrl: String? = null,
     public actual val signupRedirectUrl: String? = null,
     public actual val customScopes: List<String>? = null,
     public actual val providerParams: Map<String, String>? = null,
     public actual val oauthAttachToken: String? = null,
     public actual val sessionDurationMinutes: Int? = null,
-)
+    public val applePresentationContextProvider: ASAuthorizationControllerPresentationContextProvidingProtocol? = null,
+    public val oauthPresentationContextProvider: ASWebAuthenticationPresentationContextProvidingProtocol? = null,
+) {
+    public constructor(sessionDurationMinutes: Int) : this(null, null, null, null, null, sessionDurationMinutes, null, null)
+    public constructor(
+        loginRedirectUrl: String,
+    ) : this(loginRedirectUrl, null, null, null, null, null, null, null)
+
+    public constructor(
+        loginRedirectUrl: String,
+        signupRedirectUrl: String,
+    ) : this(loginRedirectUrl, signupRedirectUrl, null, null, null, null, null, null)
+
+    public constructor(
+        loginRedirectUrl: String,
+        signupRedirectUrl: String,
+        sessionDurationMinutes: Int,
+    ) : this(loginRedirectUrl, signupRedirectUrl, null, null, null, sessionDurationMinutes, null, null)
+
+    public constructor(
+        loginRedirectUrl: String,
+        signupRedirectUrl: String,
+        customScopes: List<String>,
+    ) : this(loginRedirectUrl, signupRedirectUrl, customScopes, null, null, null, null, null)
+
+    public constructor(
+        loginRedirectUrl: String,
+        signupRedirectUrl: String,
+        customScopes: List<String>,
+        providerParams: Map<String, String>,
+    ) : this(loginRedirectUrl, signupRedirectUrl, customScopes, providerParams, null, null, null, null)
+
+    public constructor(
+        loginRedirectUrl: String,
+        signupRedirectUrl: String,
+        customScopes: List<String>,
+        providerParams: Map<String, String>,
+        oauthAttachToken: String,
+    ) : this(loginRedirectUrl, signupRedirectUrl, customScopes, providerParams, oauthAttachToken, null, null, null)
+
+    public constructor(
+        loginRedirectUrl: String,
+        signupRedirectUrl: String,
+        customScopes: List<String>,
+        providerParams: Map<String, String>,
+        oauthAttachToken: String,
+        sessionDurationMinutes: Int,
+    ) : this(loginRedirectUrl, signupRedirectUrl, customScopes, providerParams, oauthAttachToken, sessionDurationMinutes, null, null)
+}
--- stytch-multiplatform-shared/sdk/shared/src/iosMain/kotlin/com/stytch/sdk/passkeys/PasskeysParameters.kt
+++ stytch-multiplatform-shared/sdk/shared/src/iosMain/kotlin/com/stytch/sdk/passkeys/PasskeysParameters.kt
@@ -4,4 +4,11 @@ public actual class PasskeysParameters(
     public actual val domain: String,
     public actual val sessionDurationMinutes: Int? = null,
     public actual val preferImmediatelyAvailableCredentials: Boolean = true,
-)
+) {
+    public constructor(domain: String, sessionDurationMinutes: Int) : this(domain, sessionDurationMinutes, true)
+    public constructor(
+        domain: String,
+        preferImmediatelyAvailableCredentials: Boolean,
+    ) : this(domain, null, preferImmediatelyAvailableCredentials)
+    public constructor(domain: String) : this(domain, null, true)
+}
