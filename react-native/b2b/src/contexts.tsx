defc6ec90394d9b62d6134578ea9daf98ed9acc3
Jordan Haven <jhaven@stytch.com>
2026-03-12 12:20:05 -0400

    Avoid unsafe defaults in RN hooks; throw error if used wrong

--- react-native/b2b/src/contexts.tsx
+++ react-native/b2b/src/contexts.tsx
@@ -3,5 +3,5 @@ import { StytchB2B, ApiOrganizationV1Member, ApiB2bSessionV1MemberSession, B2BAu
 
 export const StytchMemberContext = createContext<ApiOrganizationV1Member | undefined>(undefined);
 export const StytchMemberSessionContext = createContext<ApiB2bSessionV1MemberSession | undefined>(undefined);
-export const StytchB2BContext = createContext<StytchB2B>({} as StytchB2B);
+export const StytchB2BContext = createContext<StytchB2B | null>(null);
 export const StytchB2BAuthenticationStateContext = createContext<B2BAuthenticationState>(B2BAuthenticationState.Loading);
